cf7187272ea206903dc805ff855f4f3b60fe7903
antinomezco <constantino.saldana@gmail.com>
2022-04-01 20:02:31 -0400

    trying mobile screen cut off fix

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -4,10 +4,10 @@ import Header from "../ui/Header";
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="w-full">
       <Header />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   )
 }
